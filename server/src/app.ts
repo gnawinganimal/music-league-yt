cbea6af0a3b8b4b557cee9588cb6ed6bcd7ff497
gnawinganimal <unrongeur@proton.me>
2023-06-18 16:00:02 -0700

    Add env.ts file

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 
 import users from "./routes/users";
 import leagues from "./routes/leagues";
 import rounds from "./routes/rounds";
 import songs from "./routes/songs";
 
-dotenv.config();
-mongoose.connect( 
-    process.env?.DATABASE_URL ?? (() => {throw new Error("DATABASE_URL not found in env")})()
-);
+import { DATABASE_URL } from "./env";
+
+mongoose.connect(DATABASE_URL);
 
 const app = express();
 app.use(express.static("public"));
