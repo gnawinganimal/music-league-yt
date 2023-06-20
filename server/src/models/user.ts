86028f94773ecb49447cfd4544a74533fdc88492
gnawinganimal <unrongeur@proton.me>
2023-06-20 16:19:15 -0700

    Add password hashing to user schema

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,8 +1,30 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+let salt = 10;
 
 const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true }
+    name: { type: String, required: true },
+
+    email: { type: String, required: true },
+    password: { type: String, required: true },
 }, { collection: "users" });
+
+UserSchema.pre("save", function(next) {
+    if (this.isNew || this.isModified("password")) {
+        let user = this;
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) {
+                next(err);
+            } else {
+                user.password = hash;
+                next();
+            }
+        });
+    } else {
+        next();
+    }
+});
 
 const User = mongoose.model("User", UserSchema);
 
