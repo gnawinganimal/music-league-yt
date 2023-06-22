5ae4f4b2c056ac7778689a96afcc858dbdb45518
gnawinganimal <unrongeur@proton.me>
2023-06-21 18:34:17 -0700

    Add better spacing to home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -13,12 +13,12 @@ function Home() {
 
   return (
     <div id="Home">
-      <div className="px-60">
+      <div className="px-60 space-y-6">
         <h3 className="text-3xl">Your Leagues</h3>
         <div id="leagues" className="grid grid-cols-3 gap-4">
           {leagues.map((league, key) => (
-            <div key={key} className="shadow-md">
-              <h4 className="text-2xl text-purple-500">{league.name}</h4>
+            <div key={key} className="shadow-md p-6">
+              <h4 className="text-2xl text-teal-500">{league.name}</h4>
               <p className="text-gray-600">{league.desc}</p>
             </div>
           ))}
